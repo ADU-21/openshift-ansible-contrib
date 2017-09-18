4aa6a91587b6c173fb57326e1bb74720fb4bf49d
root <root@ip-172-31-39-190.ap-southeast-2.compute.internal>
2017-09-18 10:43:54 +0000

    add what I like

---
+++ reference-architecture/aws-ansible/yidong_is_handsome.sh
@@ -0,0 +1,17 @@
+export AWS_ACCESS_KEY_ID=AKIAIGEI5YZMXY55W54Q
+export AWS_SECRET_ACCESS_KEY=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
+
+./ose-on-aws.py \
+--keypair=yidong_sydney \
+--public-hosted-zone=oc-tw.net \
+--deployment-type=origin \
+--ami=ami-ccecf5af \
+--github-client-secret=4189c40dfa0a311881fcd808f0cfeb51bc9e6939 \
+--github-organization=ThoughtWorks-Chengdu-DevOps-Club \
+--github-client-id=84dd0bf53dbfc4759baa \
+--region=ap-southeast-2 \
+--key-path=/root/.ssh/yidong_sydney.pem \
+--openshift-metrics-storage-volume-size=10Gi \
+--openshift-logging-storage-volume-size=20Gi \
+--containerized=true \
+--bastion-sg=s-2ecf1c48
